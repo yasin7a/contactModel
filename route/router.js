0cc1adadb1e2a2cee591e38bd9f28eae024cd8c5
Yasin <arafath7yasin@gmal.com>
2022-01-12 16:08:58 -0800

    v2

--- route/router.js
+++ route/router.js
@@ -11,8 +11,8 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { name, email, phone, id } = req.body;
-  
+  const { name, email, phone } = req.body;
+
   try {
     const error = {};
     if (!name) {
@@ -36,6 +36,55 @@ router.post("/", async (req, res) => {
       });
     }
 
+    let contact = new Contact({
+      name,
+      email,
+      phone,
+    });
+    await contact.save();
+    let contacts = await Contact.find();
+    return res.render("index", { contacts, error: {}, data: {} });
+  } catch (error) {
+    console.log(" router.post ~ error", error);
+  }
+});
+
+router.post("/edit", async (req, res) => {
+  const { name, email, phone, id } = req.body;
+
+  try {
+    const error = {};
+    let contacts = await Contact.findOne({ _id: id });
+
+    if (!name) {
+      error.name = "Name is required";
+      contacts.name = "";
+    } else {
+      contacts.name = name;
+    }
+    if (!email) {
+      error.email = "Email is required";
+      contacts.email = "";
+    } else {
+      contacts.email = email;
+    }
+    if (!phone) {
+      error.phone = "Phone is required";
+      contacts.phone = "";
+    } else {
+      contacts.phone = phone;
+    }
+
+    const isError = Object.keys(error).length > 0;
+
+    if (isError) {
+      return res.render("edit", {
+        contacts,
+        error,
+        data: {},
+      });
+    }
+
     if (id) {
       await Contact.findByIdAndUpdate(
         { _id: id },
@@ -50,34 +99,23 @@ router.post("/", async (req, res) => {
           new: true,
         }
       );
-
-      let contacts = await Contact.find();
-      return res.render("index", { contacts, error: {}, data: {} });
-    } else {
-      let contact = new Contact({
-        name,
-        email,
-        phone,
-      });
-      await contact.save();
-      let contacts = await Contact.find();
-      return res.render("index", { contacts, error: {}, data: {} });
+      return res.redirect("/");
     }
   } catch (error) {
     console.log(" router.post ~ error", error);
   }
 });
 
-// router.get("/:id", async (req, res) => {
-//   try {
-//     let { id } = req.params;
+router.get("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
 
-//     let contacts = await Contact.findOne({ _id: id });
-//     return res.json({ contacts });
-//   } catch (error) {
-//     console.log(" router.get ~ error", error);
-//   }
-// });
+    let contacts = await Contact.findOne({ _id: id });
+    res.render("edit", { contacts, error: {}, data: {} });
+  } catch (error) {
+    console.log(" router.get ~ error", error);
+  }
+});
 
 router.get("/delete/:id", async (req, res) => {
   try {
